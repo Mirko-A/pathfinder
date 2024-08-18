4ed619f85f1aff4785c2e4561fa7571bba5c1319
Mirko-A <adzicmirko97@gmail.com>
2024-08-18 12:37:26 +0200

    Go back to default app cfg

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ mod egui_app;
 mod grid;
 
 fn main() -> eframe::Result<()> {
-    //    let app = egui_app::Pathfinder::default();
-    let app = egui_app::Pathfinder::new(20, 20, 50);
+    let app = egui_app::Pathfinder::default();
     egui_app::Pathfinder::run(app)
 }
